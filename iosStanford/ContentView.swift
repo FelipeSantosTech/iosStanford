b26cfcbf895c0f8730c68ec99f981cac15499525
FelipeSantosTech <felpsantos.c@gmail.com>
2026-01-23 13:20:43 -0300

    finished lesson 1

--- iosStanford/ContentView.swift
+++ iosStanford/ContentView.swift
@@ -8,17 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
+    var body: some View {//we use leading and trailing instead of left and right. spacing can be negative
+        VStack(alignment: .leading, spacing: -7) {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello,some suckers paid for this shit!")
             Text("I'm getting it for free")
             Text("I'm joking ofc")
+            Image(systemName: "checklist")// thats how you call images from the library
+                .imageScale(.large)
+                .foregroundStyle(.purple)
         }
         .padding()
+        
+        ZStack{
+            Circle()
+            Image(systemName: "tree")
+                .foregroundStyle(.white)
+                .imageScale(.large)
+            
+        }
     }
+    
+    
+    
 }
 
 #Preview {
