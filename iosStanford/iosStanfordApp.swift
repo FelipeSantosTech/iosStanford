a74eb0da2ddaad61457f53faf50619f53dbff13d
FelipeSantosTech <felpsantos.c@gmail.com>
2026-01-23 11:00:45 -0300

    Initial Commit

---
+++ iosStanford/iosStanfordApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosStanfordApp.swift
+//  iosStanford
+//
+//  Created by Paulo Marcelo Santos on 23/01/26.
+//
+
+import SwiftUI
+
+@main
+struct iosStanfordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
